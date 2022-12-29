33d7cb0e5dd9133bf79f69e8bef1e657689f9c64
Navneet Singh Loomba <75477408+Navneets2121@users.noreply.github.com>
2022-12-29 19:21:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return ("!! Welcome !!")
 
 @app.route('/get', methods=['GET'])
 def get_bot_response():
